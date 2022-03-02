4d71c2b46bbbb58502e5f31a8d82d139ac6c2fd3
Damola Adediran <tylher123@gmail.com>
2022-03-02 13:42:44 +0100

    Store data for popup in js file

--- script.js
+++ script.js
@@ -10,6 +10,62 @@ const projectButtons = Array.from(document.querySelectorAll(".works"));
 const popupContainer = document.querySelector("project-popup-window");
 const wrapper = document.getElementById("wrapper");
 const body = document.getElementById("page-body");
+const projects = [
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    image: "assets/images/Snapshoot-Portfolio.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    live: "https://deyemiobaa.github.io/Personal-Portfolio/",
+    source: "https://github.com/deyemiobaa/Personal-Portfolio/",
+  },
+];
 
 hamburger.addEventListener('click', () => {
   mobileNavMenu.style.display = 'block';
