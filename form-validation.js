5733697f01f3b6bd84750078b1600c0f9c1d5a04
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-02 22:51:50 +0100

    Write script to validate form

---
+++ form-validation.js
@@ -0,0 +1,16 @@
+const email = document.getElementById('email');
+const form = document.getElementById('contact-form');
+const errorMsg = document.getElementById('form-error');
+const format = 
+form.addEventListener('submit', (event) => {
+  if (email.value !== email.value.toLowerCase()) {
+    errorMsg.style.visibility = 'visible';
+    event.preventDefault();
+  } else if (email.validity.typeMismatch) {
+    errorMsg.textContent = 'Please enter a valid email address';
+    errorMsg.style.visibility = 'visible';
+    event.preventDefault();
+  } else {
+    errorMsg.style.visibility = 'hidden';
+  }
+});
