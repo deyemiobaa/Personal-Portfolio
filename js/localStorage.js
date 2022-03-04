a223794efdcb0d04acae3de472ef6ab6e7b63ba3
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-04 17:49:59 +0100

    Add a reset button to contact form

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,6 +2,7 @@ const contactForm = document.querySelector('#contact-form');
 const fullName = document.querySelector('#name');
 const emailAddress = document.querySelector('#email');
 const message = document.querySelector('#message');
+const reset = document.getElementById('reset');
 
 function dataStorage() {
   const user = {
@@ -20,3 +21,10 @@ const userDataObject = JSON.parse(localStorage.getItem('userData'));
 fullName.value = userDataObject.userFullName;
 emailAddress.value = userDataObject.userEmail;
 message.value = userDataObject.userMessage;
+
+reset.addEventListener('click', () => {
+  localStorage.removeItem('userData');
+  fullName.value = '';
+  emailAddress.value = '';
+  message.value = '';
+});
