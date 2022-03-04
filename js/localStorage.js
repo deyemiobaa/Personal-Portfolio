53f81e52c4c9ac531e8b7172da487c6e1963e729
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-04 08:45:57 +0100

    Fix style errors

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,7 +1,7 @@
-const contactForm = document.querySelector("#contact-form");
-const fullName = document.querySelector("#name");
-const emailAddress = document.querySelector("#email");
-const message = document.querySelector("#message");
+const contactForm = document.querySelector('#contact-form');
+const fullName = document.querySelector('#name');
+const emailAddress = document.querySelector('#email');
+const message = document.querySelector('#message');
 
 function dataStorage() {
   const user = {
@@ -10,12 +10,12 @@ function dataStorage() {
     userMessage: message.value,
   };
 
-  localStorage.setItem("userData", JSON.stringify(user));
+  localStorage.setItem('userData', JSON.stringify(user));
 }
 
-contactForm.addEventListener("focusout", dataStorage);
+contactForm.addEventListener('focusout', dataStorage);
 
-const userDataObject = JSON.parse(localStorage.getItem("userData"));
+const userDataObject = JSON.parse(localStorage.getItem('userData'));
 
 fullName.value = userDataObject.userFullName;
 emailAddress.value = userDataObject.userEmail;
