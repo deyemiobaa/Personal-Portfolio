c961d19de43f996852977efe0f746dbe9e0ea547
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-12 10:29:30 +0100

    Fix style errors

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,16 @@
 const wrapper = document.getElementById('wrapper');
 const body = document.getElementById('page-body');
-const recentWorks = document.querySelector('.recent-works-cards')
-const modalbody = document.querySelector('.project-popup-window')
-const projectImage = document.querySelector('.pop-up-overlay-image')
-const projectTitle = document.querySelector('.project-title')
-const projectTechnologies = document.querySelector('.lang-tags')
-const projectDescription = document.querySelector('.description')
-const liveLink = document.querySelector('.live-view')
-const sourceCode = document.querySelector('.source-code')
-const closeProject = document.querySelector('.close')
-const previousButton = document.querySelector('.prevProject')
-const nextButton = document.querySelector('.nextProject')
+const recentWorks = document.querySelector('.recent-works-cards');
+const modalbody = document.querySelector('.project-popup-window');
+const projectImage = document.querySelector('.pop-up-overlay-image');
+const projectTitle = document.querySelector('.project-title');
+const projectTechnologies = document.querySelector('.lang-tags');
+const projectDescription = document.querySelector('.description');
+const liveLink = document.querySelector('.live-view');
+const sourceCode = document.querySelector('.source-code');
+const closeProject = document.querySelector('.close');
+const previousButton = document.querySelector('.prevProject');
+const nextButton = document.querySelector('.nextProject');
 const projectsData = [
   {
     id: 0,
@@ -75,8 +75,8 @@ const projectsData = [
 ];
 
 projectsData.forEach((project) => {
-  recentWorks.innerHTML += 
-    `
+  recentWorks.innerHTML
+    += `
     <div class="works">
       <div class="work-details">
         <h3>${project.name}</h3>
@@ -85,60 +85,57 @@ projectsData.forEach((project) => {
         </ul>
         <button id="${project.id}" class="works-button">See Project</button>
       </div>
-    </div> `
-})
-
+    </div> `;
+});
 
 function disablePreviousOrNextButton() {
   if (previousButton.id <= 0) {
-    previousButton.style.visibility = 'hidden'
+    previousButton.style.visibility = 'hidden';
   } else if (nextButton.id >= projectsData.length - 1) {
-    nextButton.style.visibility = 'hidden'
+    nextButton.style.visibility = 'hidden';
   } else {
-    previousButton.style.visibility = 'visible'
-    nextButton.style.visibility = 'visible'
+    previousButton.style.visibility = 'visible';
+    nextButton.style.visibility = 'visible';
   }
 }
 function populatePopup(object) {
-  projectImage.src = projectsData[object].image
-  projectTitle.textContent = projectsData[object].name
-  projectTechnologies.innerHTML = projectsData[object].technologies.map((tech) => (`<li>${tech}</li>`)).join('')
-  projectDescription.textContent = projectsData[object].description
-  liveLink.href = projectsData[object].live
-  sourceCode.href = projectsData[object].source
-  previousButton.id = projectsData[object].id
-  nextButton.id = projectsData[object].id
+  projectImage.src = projectsData[object].image;
+  projectTitle.textContent = projectsData[object].name;
+  projectTechnologies.innerHTML = projectsData[object].technologies.map((tech) => (`<li>${tech}</li>`)).join('');
+  projectDescription.textContent = projectsData[object].description;
+  liveLink.href = projectsData[object].live;
+  sourceCode.href = projectsData[object].source;
+  previousButton.id = projectsData[object].id;
+  nextButton.id = projectsData[object].id;
   wrapper.classList.add('blur');
   body.classList.add('color');
-  disablePreviousOrNextButton()
+  disablePreviousOrNextButton();
 }
 
 function closePopup(closeButton) {
   closeButton.addEventListener('click', () => {
-    modalbody.style.display = 'none'
+    modalbody.style.display = 'none';
     wrapper.classList.remove('blur');
     body.classList.remove('color');
-  })
+  });
 }
-closePopup(closeProject)
+closePopup(closeProject);
 
-const projectButtons = document.querySelectorAll('.works-button')
-let currentProject = 0
+const projectButtons = document.querySelectorAll('.works-button');
 projectButtons.forEach(
   (button) => button.addEventListener('click', () => {
-    populatePopup(Number(button.id))
-    modalbody.style.display = 'block'
-    currentProject = Number(button.id)
-  })
-)
+    populatePopup(Number(button.id));
+    modalbody.style.display = 'block';
+  }),
+);
 
 function goToPreviousProject() {
-  populatePopup(Number(previousButton.id) - 1)
+  populatePopup(Number(previousButton.id) - 1);
 }
 
 function goToNextProject() {
-  populatePopup(Number(nextButton.id) + 1) 
+  populatePopup(Number(nextButton.id) + 1);
 }
 
-previousButton.addEventListener('click', goToPreviousProject)
-nextButton.addEventListener('click', goToNextProject)
+previousButton.addEventListener('click', goToPreviousProject);
+nextButton.addEventListener('click', goToNextProject);
